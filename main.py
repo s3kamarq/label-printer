a3ec9f96f61e7352529ef6295d4c883930b103f7
s3kamarq <katrinmarques@gmail.com>
2024-02-20 15:11:18 +0100

    Bug Fix- Person without Code can now register & label will be printed and added in Excel

--- main.py
+++ main.py
@@ -1,136 +1,6 @@
-#import sys
-#import PySimpleGUI as sg 
-#from make_label1902 import make_label
-
-"""
-
-sg.theme('LightGrey1')   # Add a touch of color
-Druckzeit = 8 # Bitte Zeit eingeben wie lange der Drucker braucht
-
-
-with open('Teilnahmebedingungen.txt', 'r') as f: # liesst die Teilnahmebedingungen aus dem Text-File
-    Teilnahmebedingungen_Text = f.read()
-
-do_label = make_label() #creates the object from module make_label that crates the labels
-
-def anmeldung_window():
-    layout = [
-        [sg.Text('Vorname', size=(10, 1)), sg.Input(key='-Vorname-', size=(30, 1))],
-        [sg.Text('Name', size=(10, 1)), sg.Input(key='-Name-', size=(30, 1))],
-        [sg.Text('Organisation', size=(10, 1)), sg.Input(key='-Organisation-', size=(30, 1))],
-        [sg.Text('E-Mail', size=(10, 1)), sg.Input(key='QR-Code', size=(30, 1))],
-        [sg.Button('Bestätigen')]
-    ]
-
-    subwindow = sg.Window('Anmeldung', layout)
-
-    while True:
-        event, values = subwindow.read()
-        if event == sg.WINDOW_CLOSED:
-            break
-        elif event == 'Bestätigen':
-            vorname = values['-Vorname-']
-            name = values['-Name-']
-            organisation = values['-Organisation-']
-            email = values['QR-Code']
-            print(values)
-            do_label.check_entry(values['QR-Code'])
-            #window['QR-Code'].update("")
-            sg.popup('Sie haben sich erfolgreich angemeldet! Ihr Namensschild wird nun gedruckt.', auto_close=True, auto_close_duration=7)
-            break
-        elif event == "QR-Code" + "_Enter":
-            vorname = values['-Vorname-']
-            name = values['-Name-']
-            organisation = values['-Organisation-']
-            email = values['QR-Code']
-            print(values)
-            do_label.check_entry(values['QR-Code'])
-            #window['QR-Code'].update("")
-            sg.popup('Sie haben sich erfolgreich angemeldet! Ihr Namensschild wird nun gedruckt.', auto_close=True, auto_close_duration=7)
-            break
-
-    subwindow.close()
-
-sg.theme('LightGrey1')   # Füge etwas Farbe hinzu
-
-# Layout definieren
-layout = [
-    [sg.Image('banner.png', size=(None, None))],  # Banner einfügen
-    [sg.Text(' ')],  # Leerer Text für einen Zeilenumbruch
-    [sg.Text(' ')],  # Leerer Text für einen Zeilenumbruch
-    [sg.Text('Willkommen!', size=(None, None), font=('Helvetica', 20), justification='center')],  # Großer Text "Willkommen!"
-    [sg.Text('Zum Einchecken bitte Ihren QR-Code an den Scanner halten.', size=(None, None), font=('Helvetica', 20), justification='center')],  # Großer Text "Willkommen!"
-    [sg.Text(' ')],  # Leerer Text für einen Zeilenumbruch
-    [sg.Text(' ')],  # Leerer Text für einen Zeilenumbruch
-    [sg.Text(' ')],  # Leerer Text für einen Zeilenumbruch
-    [sg.Text('Noch nicht angemeldet?', size=(20, 1), justification='left'), sg.Button('Zur Anmeldung', size=(None, None), button_color=('white', 'black'), key='Anmeldung')],  # Button "Zur Anmeldung"
-]
-
-# Fenster erstellen
-window = sg.Window('Anmeldung zum Kongress 2024', layout, size=[850, 500], finalize=True, resizable=True)
-#window['QR-Code'].bind("<Return>", "_Enter")
-
-while True:  # Event Loop
-    UID = input("Bitte Scannen!")
-    do_label.check_entry(UID)
-    event, values = window.read()
-    if event == sg.WINDOW_CLOSED or event == 'Exit':
-        break
-    elif event == 'Anmeldung':  # Wenn der "Zur Anmeldung" Button geklickt wird
-        anmeldung_window()# Hier können Sie den Code hinzufügen, um das neue Fenster für die Anmeldung zu öffnen
-    else:
-        UID = input("Bitte Scannen!")
-        do_label.check_entry(UID)
-
-window.close()
-"""
-##########################################################################################
-
-"""
-layout = [
-          #[sg.Text(' ', size=(20, 1))],
-          [sg.Push(), sg.Image('bannerkm.png', size=(None,None)), sg.Push()],
-          [sg.Text(' ', size=(20, 1))],
-          [sg.Text('       Bitte Ihre QR-Anmelde-Code an dem Scanner halten oder Ihre E-mail hier eingeben',justification='center')],
-          [sg.Text('     '), sg.Input(key='QR-Code')],
-          [sg.Text(' ', size=(20, 1))],        
-          [sg.Text('       Durch Scannen der QR-Anmelde-Code oder durch das Klicken auf OK stimmen Sie die Teilnahmebedingungen zu.',justification='center')], 
-          [sg.Text('     '),sg.Button('Teilnahmebedingungen')],        
-          [sg.Text(' ', size=(20, 1))],
-          [sg.Push(), sg.Button('OK'),sg.Button('Exit'), sg.Push()]#,
-          #[sg.Text(' ', size=(20, 1))]
-          ,]
-
-
-window = sg.Window('Anmeldung zum Kongress 2024', layout, size=[850,500], finalize=True, resizable=True)
-#window.Maximize()
-window['QR-Code'].bind("<Return>", "_Enter")
-
-while True:  # Event Loop
-    event, values = window.read()
-    #mailadress= input('Scan now: ')
-    #do_label.check_entry(mailadress)
-    #print(event, values)
-    if event == sg.WIN_CLOSED or event == 'Exit':
-        break
-    if event == 'Teilnahmebedingungen':
-        sg.popup(Teilnahmebedingungen_Text)
-    elif event == "QR-Code" + "_Enter":
-        print(values)
-        window['QR-Code'].update("")
-        do_label.check_entry(values['QR-Code'])
-
-    elif event == "OK":
-        print(values)
-        do_label.check_entry(values['QR-Code'])
-        window['QR-Code'].update("")
-
-window.close()
-"""
-##########################################################################################################
-################################################################################################
 import PySimpleGUI as sg 
 from make_label1902 import make_label
+import pandas as pd
 
 sg.theme('LightGrey1')
 
@@ -138,6 +8,27 @@ with open('Teilnahmebedingungen.txt', 'r') as f:
     teilnahmebedingungen_text = f.read()
 
 do_label = make_label()
+
+import openpyxl
+
+def add_to_excel(vorname, name, organisation, email, excel_file):
+    # Öffne die Excel-Datei
+    wb = openpyxl.load_workbook(excel_file)
+    sheet = wb.active
+    
+    # Finde die nächste leere Zeile
+    next_row = sheet.max_row + 1
+    
+    # Füge die Daten in die nächste leere Zeile ein
+    sheet.cell(row=next_row, column=2).value = vorname
+    sheet.cell(row=next_row, column=3).value = name
+    sheet.cell(row=next_row, column=4).value = organisation
+    sheet.cell(row=next_row, column=5).value = email
+    
+    # Speichere die Änderungen
+    wb.save(excel_file)
+
+
 
 def anmeldung_window():
     layout = [
@@ -159,6 +50,7 @@ def anmeldung_window():
             name = values['-Name-']
             organisation = values['-Organisation-']
             email = values['email']
+            add_to_excel(vorname=vorname,name=name,organisation=organisation, email=email,excel_file='Teilnehmerliste_Final.xlsx')
             do_label.check_entry(email)
             sg.popup('Sie haben sich erfolgreich angemeldet! Ihr Namensschild wird nun gedruckt.', auto_close=True, auto_close_duration=7)
             break
@@ -174,8 +66,8 @@ layout = [
     [sg.Text(' ')],
     [sg.Text(' ')],
     [sg.Text(' ')],
-    [sg.Text('Ihre E-Mail:', size=(10, 1)), sg.Input(key='QR-Code', size=(30, 1), enable_events=True, focus=True)],  # QR-Code-Feld mit Ereignisaktivierung
-    [sg.Button('Anmelden')],
+    [sg.Text('Ihre Ticket-Code:', size=(20, 1)), sg.Input(key='QR-Code', size=(30, 1), enable_events=True, focus=True)],  # QR-Code-Feld mit Ereignisaktivierung
+    [sg.Text('Noch nicht angemeldet?', size=(20, 1), justification='left'), sg.Button('Zur Anmeldung', size=(None, None), button_color=('white', 'black'), key='Anmeldung')],  # Button "Zur Anmeldung",
 ]
 
 window = sg.Window('Anmeldung zum Kongress 2024', layout, size=[850, 500], finalize=True, resizable=True)
